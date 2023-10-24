a81dc48c462f8c3154910c50503e7df5a68f36b3
Vishu-221b <vishwajeetsinghchauhan63@gmail.com>
2023-10-24 21:04:34 +0530

    raw noteflow ready

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,23 +1,30 @@
-export default function Sidebar(props){
+import React from "react"
 
-    const noteElements = props.notes.map((note, index) => {
-        return(
-            <div key={note.id}>
-            <div>
-                <h2>hi this is a note.</h2>
-                <button onClick={props.deleteNote}>Delete this note</button>
+export default function Sidebar(props) {
+    const noteElements = props.notes.map((note, index) => (
+        <div key={note.id}>
+            <div
+                className={`title ${note.id === props.currentNote.id ? "selected-note" : ""}`}
+                onClick={() => props.setCurrentNoteId(note.id)}
+            >
+                <h4 className="text-snippet">{note.body.split("\n")[0]}</h4>
+                <button 
+                    className="delete-btn"
+                    onClick={(event) => props.deleteNote(event, note.id)}
+                >
+                <i className="gg-trash trash-icon"></i>
+                </button>
             </div>
-            </div>
-        )
-    });
+        </div>
+    ))
 
-    return(
-        <section>
-    <div>
-        <h3>Notes</h3>
-        <button onClick={props.createNewNote}>+</button>
-    </div>
-    {noteElements}
-    </section>
-)
+    return (
+        <section className="pane sidebar">
+            <div className="sidebar--header">
+                <h3>Notes</h3>
+                <button className="new-note" onClick={props.newNote}>+</button>
+            </div>
+            {noteElements}
+        </section>
+    )
 }
